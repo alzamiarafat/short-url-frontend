80d7a779bdc643d5cb228d98ebbcbb7f553abe9a
alzamiarafat <alzamiarafat00@gmail.com>
2024-09-20 01:37:21 +0600

    home page design

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -41,6 +41,12 @@ const Home = () => {
                         <div class="flex justify-around">
                             <span class="font-medium">{shortUrl}</span>
                             <button class="font-medium" onClick={() => copy()}>Copy</button>
+                            <button type="button" data-dismiss-target="#toast-success" aria-label="Close" onClick={() => setIsShowShortUrl(false)}>
+                                <span class="sr-only">Close</span>
+                                <svg class="w-3 h-3" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 14 14">
+                                    <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="m1 1 6 6m0 0 6 6M7 7l6-6M7 7l-6 6" />
+                                </svg>
+                            </button>
                         </div>
                     </div>
                 </div>
